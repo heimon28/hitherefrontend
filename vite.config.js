00c51390a00bacf358cc0f45496bfcf62a263a2a
heimon1255@gmail.com <heimon28@gmail.com>
2024-07-20 19:02:37 +0530

    change vite config and add backend url

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   server: {
     proxy: {
-      '/api': 'https://hithere-6wrl.onrender.com'
+      '/api': 'https://news-app-15hj.onrender.com'
 
     }
   },
